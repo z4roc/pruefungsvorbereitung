31a00de75bc6c89dea66a3eed6dab03e721ce00d
z4roc <arthuraktamirov@gmail.com>
2023-06-07 19:28:05 +0200

    locales

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Prüfungsvorbereitung</span>,
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - Prüfungsvorbereitung",
+    };
+  },
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/z4roc/pruefungsvorbereitung",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.gg/usS8XgVYnF",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/z4roc/pruefungsvorbereitung",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Prüfungsvorbereitung",
   },
-}
+  i18n: [{ locale: "de", text: "Deutsch" }],
+  search: {
+    placeholder: "Themen oder Inhalte suchen...",
+  },
+};
 
-export default config
+export default config;
